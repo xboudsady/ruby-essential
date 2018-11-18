398aea7d9e52010c26e8bd68514a1c4ce813d4c4
xboudsady <boudsadyx@gmail.com>
2018-11-17 17:20:56 -0800

    Checkpoint

---
+++ symbols.rb
@@ -0,0 +1,15 @@
+puts :test
+
+puts :this_text
+
+puts "test".object_id
+
+puts :test.object_id
+
+puts "test".object_id
+
+puts :test.object_id
+
+puts hash = {:first_namej => 'Kevin', :last_name => 'Skoglund'}
+
+puts hash['first_name']
