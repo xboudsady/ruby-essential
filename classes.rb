930f0d9fbd754a9fe50d109616dd8f16b2f9975d
xboudsady <boudsadyx@gmail.com>
2018-11-17 17:41:04 -0800

    reader and writer methods

--- classes.rb
+++ classes.rb
@@ -1,17 +1,37 @@
 class Animal
-    def set_noise(noise)
+    attr_accessor :name
+    attr_writer :color
+    attr_reader :legs, :arms
+
+    def setup_limbs
+        @legs = 4
+        @arms = 0
+    end
+
+    def noise=(noise)
         @noise = noise
     end
 
-    def make_noise
+    def noise
         @noise
+    end
+
+    def color
+        "The color is #{@color}"
     end
 end
 
 animal1 = Animal.new
-animal1.set_noise("Moo!")
-puts animal1.make_noise
+animal1.setup_limbs
+animal1.noise = "Moo!"
+animal1.name = "Steve"
+puts animal1.name
+animal1.color = "black"
+puts animal1.color
+puts animal1.legs
+
+puts animal1.noise
 
 animal2 = Animal.new
-animal2.set_noise("Quack!")
-puts animal2.make_noise
+animal2.noise = "Quack!"
+puts animal2.noise
