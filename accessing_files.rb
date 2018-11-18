af93c58aa22ac9b2e54c819d66f9e922f010e21f
xboudsady <boudsadyx@gmail.com>
2018-11-18 12:51:15 -0800

    Creating files and accessing files

--- accessing_files.rb
+++ accessing_files.rb
@@ -10,7 +10,15 @@ file.close
 
 # 2. Provide a file with a block of instructions
 File.open('file1.txt', 'r') do |file|
-    # read data from file
+    while line = file.gets
+        puts "** " + line.chomp.reverse + " **"
+    end
+end
+
+# if you know you want to read every line,
+# each_line will be easier
+File.open('file1.txt', 'r') do |file|
+    file.each_line { |line| puts line.upcase }
 end
 
 # second argument is the 'mode': r, w, a, r+, w+, a+
