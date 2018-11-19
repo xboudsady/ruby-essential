920069e3eeed303ea93a239cfee8893d002adb16
xboudsady <boudsadyx@gmail.com>
2018-11-18 18:44:50 -0800

    Completed code

--- food_finder/init.rb
+++ food_finder/init.rb
@@ -1,14 +1,14 @@
-### Food Finder ###
+#### Food Finder ####
 #
-# Launch this Ruby file from the commnand line
-# to get started
+# Launch this Ruby file from the command line 
+# to get started.
 #
 
 APP_ROOT = File.dirname(__FILE__)
 
 # require "#{APP_ROOT}/lib/guide"
-# require FILE.join(APP_ROOT, 'lib', 'guide.rb')
-# require FILE.join(APP_ROOT, 'lib', 'guide')
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
 
 $:.unshift( File.join(APP_ROOT, 'lib') )
 require 'guide'
